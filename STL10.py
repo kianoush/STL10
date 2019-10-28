eba25b8a116dbdf27a188c6294a19860cc988f5b
Kian <k.karimipor@gmail.com>
2019-10-27 19:01:57 -0700

    model evaluate

--- STL10.py
+++ STL10.py
@@ -22,20 +22,17 @@ print('Cuda', gpu_cuda)
 """
 Var
 """
-batch_size = 1
+batch_size = 32
 
 
 """
 Load data
 """
 
-train_datasets = torchvision.datasets.STL10("./Datasets", split='train', transform=transforms.Compose([
-                                                                                transforms.Grayscale(),
-                                                                                transforms.ToTensor()
-                                                                            ]),
+train_datasets = torchvision.datasets.STL10("./Datasets", split='train', transform=transforms.ToTensor(),
                                                                                 download=False)
 test_datasets = torchvision.datasets.STL10("./Datasets", split='test', transform=transforms.ToTensor(),
-                                             download=False)
+                                                                                download=False)
 
 train_dl = torch.utils.data.DataLoader(train_datasets, batch_size=batch_size, shuffle=True)
 test_dl = torch.utils.data.DataLoader(test_datasets, batch_size=batch_size, shuffle=True)
@@ -48,7 +45,8 @@ Model
 
 class SimpleCNN(nn.Module):
     def __init__(self):
-        self.layer1 = nn.Sequential(nn.Conv2d(1,16,3,1,2),
+        super(SimpleCNN, self).__init__()
+        self.layer1 = nn.Sequential(nn.Conv2d(3,16,3,1,2),
                                     nn.BatchNorm2d(16),
                                     nn.ReLU(),
                                     nn.MaxPool2d(2,2))
@@ -64,6 +62,7 @@ class SimpleCNN(nn.Module):
         out2 = self.layer2(out1)
         out2 = out2.view(out2.size(0), -1)
         y = self.fc(out2)
+        return y
 
 
 
@@ -86,28 +85,28 @@ optim = optim.Adam(model.parameters(), lr=0.001)
 lr_sch = torch.optim.lr_scheduler.StepLR(optim, 1, gamma=0.5)
 
 
-
 def to_var(x, volatile=False):
     if gpu_cuda:
-        x = x.cuda
+        x = x.cuda()
     return Variable(x, volatile=volatile)
+
 
 epochs_num = 10
 losses = []
-for epoch in epochs_num:
+for epoch in range(epochs_num):
 
     model.train()
-    for i, (input, target) in enumerate(train_dl):
+    for i, (inputs, targets) in enumerate(train_dl):
 
-        inputs = to_var(input)
-        targets = to_var(target)
+        inputs = to_var(inputs)
+        targets = to_var(targets)
 
         # forward
         optim.zero_grad()
-        output = model(input)
+        output = model(inputs)
 
-        # loss
-        loss = criterions(output, target)
+        # Loss
+        loss = criterions(output, targets)
         losses += [loss.data.item()]
 
         # backward
@@ -116,6 +115,7 @@ for epoch in epochs_num:
         # update parameters
         optim.step()
 
+
         if (i+1) % 100 == 0:
             print('Train, Epoch [%2d/%2d], Step [%3d/%3d], Loss: %.4f'
                   % (epoch + 1, epochs_num, i + 1, len(train_dl), loss.data.item()))
@@ -123,4 +123,42 @@ for epoch in epochs_num:
 
 
 
-print("END!")
+
+"""
+train data
+"""
+
+model.eval()
+corrects = 0
+for j, (inputs, targets) in enumerate(train_dl):
+
+    inputs = to_var(inputs)
+    targets = to_var(targets)
+    output = model(inputs)
+    predict = torch.argmax(output, 1)
+    corrects += torch.sum(predict==targets)
+
+print(100 * corrects.data.item()/(len(test_datasets)))
+
+
+print('End!')
+
+
+"""
+Test data
+"""
+
+model.eval()
+corrects = 0
+for j, (inputs, targets) in enumerate(test_dl):
+
+    inputs = to_var(inputs)
+    targets = to_var(targets)
+    output = model(inputs)
+    predict = torch.argmax(output, 1)
+    corrects += torch.sum(predict==targets)
+
+print(100 * corrects.data.item()/(len(test_datasets)))
+
+
+print('End!')
